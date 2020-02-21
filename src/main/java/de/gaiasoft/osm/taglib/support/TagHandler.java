6e65dff42224770ddfbb64b63c6fe10ad0ecf93a
Timo Boegner <tboegner@googlemail.com>
2020-02-21 17:44:49 +0100

    Upgrade to Java 11. Improved readme (now markdown). Javadoc for TagHandler.

--- src/main/java/de/gaiasoft/osm/taglib/support/TagHandler.java
+++ src/main/java/de/gaiasoft/osm/taglib/support/TagHandler.java
@@ -1,13 +1,16 @@
 package de.gaiasoft.osm.taglib.support;
 
 
-import de.gaiasoft.osm.taglib.base.KeyBase;
 import de.gaiasoft.osm.taglib.base.ValueBase;
 import de.gaiasoft.osm.taglib.def.Key;
 
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Singleton helper handling known keys and values from OSM-tags. Provides finder methods which return
+ * results as enum-values.
+ */
 public class TagHandler {
     private static TagHandler instance = null;
     private Map<String, KeyInfo> keyCache = new HashMap<>();
@@ -30,6 +33,10 @@ public class TagHandler {
     }
 
 
+    /**
+     * Singleton instance creator.
+     * @return Singleton TagHandler instance.
+     */
     public static TagHandler getInstance() {
         if(instance == null) {
             instance = new TagHandler();
@@ -37,16 +44,32 @@ public class TagHandler {
         return instance;
     }
 
+    /**
+     * Finder for supported OSM-Keys
+     * @param key Key of OSM-Tag.
+     * @return KeyInfo-Structure for supported key, otherwise null.
+     */
     public KeyInfo findKey(String key) {
         return keyCache.get(key);
     }
 
-    // TODO: Exception, if key not exists?
+    /**
+     * Checker for known/limited values existing for a key.
+     * @param key Key of OSM-Tag.
+     * @return True, if key has known values. False, if key not supported or it has no known values.
+     */
     public boolean hasKeyLimitedValues(String key) {
         KeyInfo keyInfo = findKey(key);
         return keyInfo != null && keyInfo.valueEnum != null;
     }
 
+    /**
+     * Finder for supported/known values of a OSM-Key.
+     * @param key Key of OSM-Tag.
+     * @param value Value of OSM-Tag.
+     * @return Instance of (Enum-)Value, if value is known for key. Null if key is not supported
+     * or value is not known for key.
+     */
     public ValueBase findValidValueForKey(String key, String value) {
         KeyInfo keyInfo = findKey(key);
         if(keyInfo != null && keyInfo.valueEnum != null) {
@@ -60,6 +83,13 @@ public class TagHandler {
         return null;
     }
 
+    /**
+     * Checker for a value is valid/known for a key.
+     * @param key Key of OSM-Tag.
+     * @param value Value of OSM-Tag.
+     * @return True, if value is known/valid for key. False, if key is not supported or value is
+     * not known for key.
+     */
     // TODO: Exception, if values are not limited
     public boolean isValueValidForKey(String key, String value) {
         return findValidValueForKey(key, value) != null;
